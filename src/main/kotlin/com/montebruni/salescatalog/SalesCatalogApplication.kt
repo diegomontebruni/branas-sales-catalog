85fa27082bd33f8338e2a9d097b18a9b0ee1e79b
Diego Montebruni <montebrunidiego@gmail.com>
2023-05-25 19:57:57 -0300

    initial setup

---
+++ src/main/kotlin/com/montebruni/salescatalog/SalesCatalogApplication.kt
@@ -0,0 +1,11 @@
+package com.montebruni.salescatalog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SalesCatalogApplication
+
+fun main(args: Array<String>) {
+	runApplication<SalesCatalogApplication>(*args)
+}
---
+++ src/test/kotlin/com/montebruni/salescatalog/SalesCatalogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.montebruni.salescatalog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SalesCatalogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
